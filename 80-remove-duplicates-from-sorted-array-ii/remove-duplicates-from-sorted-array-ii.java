1b3d63f0c36f76b95c37f1a9f8b00a4eb88924eb
Vartika Dhochak <dhochakvartika@gmail.com>
2026-05-01 22:29:30 +0530

    Time: 0 ms (100.00%) | Memory: 48.8 MB (50.99%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length <= 2) return nums.length;
+        
+        int i=2; //store unique element
+        for(int j=2;j<nums.length;j++){
+            if(nums[j]!=nums[i-2]){
+                
+                nums[i]=nums[j];
+                i++;
+            }
+        }return i;
+    }
+}
